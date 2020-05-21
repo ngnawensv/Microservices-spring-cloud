91b9f1601a02cd955c73b43bb6a0b66f5acff301
ngnawen <ngnawens@gmail.com>
2020-05-21 18:06:48 +0100

    Configuration du circuit breaker avec hystrix

--- Gateway-service/src/main/java/cm/belrose/Gatewayservice/GatewayServiceApplication.java
+++ Gateway-service/src/main/java/cm/belrose/Gatewayservice/GatewayServiceApplication.java
@@ -1,5 +1,7 @@
 package cm.belrose.Gatewayservice;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
@@ -7,9 +9,13 @@ import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitio
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableHystrix
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
@@ -36,6 +42,8 @@ public class GatewayServiceApplication {
                 .addRequestHeader("x-rapidapi-host", "restcountries-v1.p.rapidapi.com")
                 .addRequestHeader("x-rapidapi-key", "e07ef331c1msh07e4e088f3c24afp1743dajsn03e96c5bf98b")
                 .rewritePath("/restcountries/(?<segment>.*)", "/${segment}")
+                .hystrix(h -> h.setName("restcountries")
+                .setFallbackUri("forward:/restcountriesFallback"))
                 )
                 .uri("https://restcountries-v1.p.rapidapi.com/all").id("countries")
                 )
@@ -44,10 +52,30 @@ public class GatewayServiceApplication {
                 .addRequestHeader("x-rapidapi-host", "world-population.p.rapidapi.com")
                 .addRequestHeader("x-rapidapi-key", "e07ef331c1msh07e4e088f3c24afp1743dajsn03e96c5bf98b")
                 .rewritePath("/world-population/(?<segment>.*)", "/${segment}")
+                .hystrix(h -> h.setName("world-population")
+                .setFallbackUri("forward:/restworlpopulationFallback"))
                 )
                 .uri("https://world-population.p.rapidapi.com/allcountriesname").id("world-population")
                 )
                 .build();
     }
 
+}
+
+@RestController
+class FallBackRestController {
+    @GetMapping("/restcountriesFallback")
+    public Map<String,String> restcountriesFallback(){
+        Map<String,String> map= new HashMap<>();
+        map.put("Message", "Default rest countries fallback service");
+        map.put("Countries", "Cameroun, Tchad");
+        return map;
+    }
+    @GetMapping("/restworlpopulationFallback")
+    public Map<String,String> restworlpopulationFallback(){
+        Map<String,String> map= new HashMap<>();
+        map.put("Message", "Default rest wolrd population fallback service");
+        map.put("Countries", "Cameroun");
+        return map;
+    }
 }
