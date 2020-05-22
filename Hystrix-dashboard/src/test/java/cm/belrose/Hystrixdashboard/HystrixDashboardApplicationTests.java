bbfcb820a84b3ff7d5d94aaa12307032926014b9
ngnawen <ngnawens@gmail.com>
2020-05-22 01:38:48 +0100

    Creation et configuration du microservice HystrixDashbord

---
+++ Hystrix-dashboard/src/test/java/cm/belrose/Hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.belrose.Hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
